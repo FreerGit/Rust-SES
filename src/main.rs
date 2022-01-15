491bdf6ba795bdbe93beb36f5279953bdfe7c67c
FreerGit <fredrik-98@live.se>
2022-01-15 03:36:55 +0100

    feat: add healt_check endpoint with tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("localhost:8000").expect("Failed to bind to port.");
+    let _ = rust_ses::run(listener).expect("Failed to bind address");
+    Ok(())
 }
