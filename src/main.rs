95b3c08998c0547848f8011be573038b00a6e1ff
FreerGit <fredrik-98@live.se>
2022-02-03 02:29:36 +0100

    feat: pgpool, database isolation per test

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use rust_ses::configuration::get_configuration;
 use rust_ses::startup::run;
+use sqlx::Connection;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 // use startup;
@@ -7,7 +9,10 @@ use std::net::TcpListener;
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     let configuration = get_configuration().expect("Failed to read configuration");
+    let connection = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
