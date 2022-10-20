2b4a4fdcea2d91b34e0a749d8e349b0114e0071f
FreerGit <fredrik-98@live.se>
2022-10-20 22:19:58 +0200

    feat: complete tracing and logging

--- src/startup.rs
+++ src/startup.rs
@@ -3,19 +3,20 @@ use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
+use tracing_actix_web::TracingLogger;
 
 use crate::routes::{health_check, subscribe};
 
 pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io::Error> {
-  let connection = web::Data::new(connection);
-  let server = HttpServer::new(move || {
-    App::new()
-      .wrap(Logger::default())
-      .route("/health_check", web::get().to(health_check))
-      .route("/subscriptions", web::post().to(subscribe))
-      .app_data(connection.clone())
-  })
-  .listen(listener)?
-  .run();
-  Ok(server)
+    let connection = web::Data::new(connection);
+    let server = HttpServer::new(move || {
+        App::new()
+            .wrap(TracingLogger::default())
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection.clone())
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
 }
