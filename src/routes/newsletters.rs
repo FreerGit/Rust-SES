45e2b3add716f0b692164d1ff66594cb73e0d0be
Fredrik Eriksson <Freer320@student.liu.se>
2022-12-17 23:47:17 +0100

    basic auth

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,33 +1,27 @@
-use actix_web::{
-    http::header::{self, HeaderMap},
-    web, HttpRequest, HttpResponse, ResponseError,
+use crate::domain::SubscriberEmail;
+use crate::email_client::EmailClient;
+use crate::routes::error_chain_fmt;
+use crate::telemetry::spawn_blocking_with_tracing;
+use actix_web::http::{
+    header::{HeaderMap, HeaderValue},
+    StatusCode,
 };
+use actix_web::{web, HttpRequest, HttpResponse, ResponseError};
 use anyhow::Context;
-use reqwest::{header::HeaderValue, StatusCode};
-use secrecy::Secret;
-use serde::Deserialize;
-use sha3::Digest;
+use argon2::{Argon2, PasswordHash, PasswordVerifier};
+use secrecy::{ExposeSecret, Secret};
 use sqlx::PgPool;
 
-use crate::{domain::SubscriberEmail, email_client::EmailClient};
-
-use super::error_chain_fmt;
-use secrecy::ExposeSecret;
-
-#[derive(Deserialize)]
+#[derive(serde::Deserialize)]
 pub struct BodyData {
     title: String,
     content: Content,
 }
 
-#[derive(Deserialize)]
+#[derive(serde::Deserialize)]
 pub struct Content {
     html: String,
     text: String,
-}
-
-struct ConfirmedSubscriber {
-    email: SubscriberEmail,
 }
 
 #[derive(thiserror::Error)]
@@ -43,6 +37,7 @@ impl std::fmt::Debug for PublishError {
         error_chain_fmt(self, f)
     }
 }
+
 impl ResponseError for PublishError {
     fn error_response(&self) -> HttpResponse {
         match self {
@@ -54,50 +49,122 @@ impl ResponseError for PublishError {
                 let header_value = HeaderValue::from_str(r#"Basic realm="publish""#).unwrap();
                 response
                     .headers_mut()
-                    // actix_web::http::header provides a collection of constants
-                    // for the names of several well-known/standard HTTP headers
-                    .insert(header::WWW_AUTHENTICATE, header_value);
+                    .insert(actix_web::http::header::WWW_AUTHENTICATE, header_value);
                 response
             }
         }
     }
 }
 
-impl std::fmt::Display for SubscriberEmail {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        // We just forward to the Display implementation of
-        // the wrapped String.
-        self.0.fmt(f)
-    }
+struct Credentials {
+    username: String,
+    password: Secret<String>,
 }
 
+fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
+    // The header value, if present, must be a valid UTF8 string
+    let header_value = headers
+        .get("Authorization")
+        .context("The 'Authorization' header was missing")?
+        .to_str()
+        .context("The 'Authorization' header was not a valid UTF8 string.")?;
+    let base64encoded_credentials = header_value
+        .strip_prefix("Basic ")
+        .context("The authorization scheme was not 'Basic'.")?;
+    let decoded_credentials = base64::decode_config(base64encoded_credentials, base64::STANDARD)
+        .context("Failed to base64-decode 'Basic' credentials.")?;
+    let decoded_credentials = String::from_utf8(decoded_credentials)
+        .context("The decoded credential string is valid UTF8.")?;
+
+    let mut credentials = decoded_credentials.splitn(2, ':');
+    let username = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A username must be provided in 'Basic' auth."))?
+        .to_string();
+    let password = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A password must be provided in 'Basic' auth."))?
+        .to_string();
+
+    Ok(Credentials {
+        username,
+        password: Secret::new(password),
+    })
+}
+
+#[tracing::instrument(name = "Get stored credentials", skip(username, pool))]
+async fn get_stored_credentials(
+    username: &str,
+    pool: &PgPool,
+) -> Result<Option<(uuid::Uuid, Secret<String>)>, anyhow::Error> {
+    let row = sqlx::query!(
+        r#"
+        SELECT user_id, password_hash
+        FROM users
+        WHERE username = $1
+        "#,
+        username,
+    )
+    .fetch_optional(pool)
+    .await
+    .context("Failed to performed a query to retrieve stored credentials.")?
+    .map(|row| (row.user_id, Secret::new(row.password_hash)));
+    Ok(row)
+}
+
+#[tracing::instrument(name = "Validate credentials", skip(credentials, pool))]
 async fn validate_credentials(
     credentials: Credentials,
     pool: &PgPool,
 ) -> Result<uuid::Uuid, PublishError> {
-    let password_hash = sha3::Sha3_256::digest(credentials.password.expose_secret().as_bytes());
-    let password_hash = format!("{:x}", password_hash);
-    let user_id: Option<_> = sqlx::query!(
-        r#"
-        SELECT user_id
-        FROM users
-        WHERE username = $1 AND password_hash = $2
-        "#,
-        credentials.username,
-        password_hash
-    )
-    .fetch_optional(pool)
+    let mut user_id = None;
+    let mut expected_password_hash = Secret::new(
+        "$argon2id$v=19$m=15000,t=2,p=1$\
+gZiV/M1gPc22ElAH/Jh1Hw$\
+CWOrkoo7oJBQ/iyh7uJ0LO2aLEfrHwTWllSAxT0zRno"
+            .to_string(),
+    );
+    if let Some((stored_user_id, stored_password_hash)) =
+        get_stored_credentials(&credentials.username, &pool)
+            .await
+            .map_err(PublishError::UnexpectedError)?
+    {
+        user_id = Some(stored_user_id);
+        expected_password_hash = stored_password_hash;
+    }
+    spawn_blocking_with_tracing(move || {
+        verify_password_hash(expected_password_hash, credentials.password)
+    })
     .await
-    .context("Failed to perform a query to validate auth credentials.")
-    .map_err(PublishError::UnexpectedError)?;
-    user_id
-        .map(|row| row.user_id)
-        .ok_or_else(|| anyhow::anyhow!("Invalid username or password."))
+    .context("Failed to spawn blocking task.")
+    .map_err(PublishError::UnexpectedError)??;
+
+    user_id.ok_or_else(|| PublishError::AuthError(anyhow::anyhow!("Unknown username.")))
+}
+
+#[tracing::instrument(
+    name = "Validate credentials",
+    skip(expected_password_hash, password_candidate)
+)]
+fn verify_password_hash(
+    expected_password_hash: Secret<String>,
+    password_candidate: Secret<String>,
+) -> Result<(), PublishError> {
+    let expected_password_hash = PasswordHash::new(expected_password_hash.expose_secret())
+        .context("Failed to parse hash in PHC string format.")
+        .map_err(PublishError::UnexpectedError)?;
+
+    Argon2::default()
+        .verify_password(
+            password_candidate.expose_secret().as_bytes(),
+            &expected_password_hash,
+        )
+        .context("Invalid password.")
         .map_err(PublishError::AuthError)
 }
 
 #[tracing::instrument(
-    name = "Publish a newsletter issue",
+name = "Publish a newsletter issue", 
     skip(body, pool, email_client, request),
     fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
 )]
@@ -114,7 +181,6 @@ pub async fn publish_newsletter(
 
     let subscribers = get_confirmed_subscribers(&pool).await?;
     for subscriber in subscribers {
-        // The compiler forces us to handle both the happy and unhappy case!
         match subscriber {
             Ok(subscriber) => {
                 email_client
@@ -126,23 +192,23 @@ pub async fn publish_newsletter(
                     )
                     .await
                     .with_context(|| {
-                        format!("Failed to send newsletter issue to {}", subscriber.email)
+                        format!("Failed to send newsletter issue to {:#?}", subscriber.email)
                     })?;
             }
             Err(error) => {
                 tracing::warn!(
-                // We record the error chain as a structured field
-                // on the log record.
-                error.cause_chain = ?error,
-                // Using `\` to split a long string literal over
-                // two lines, without creating a `\n` character.
-                "Skipping a confirmed subscriber. \
-                Their stored contact details are invalid",
+                    error.cause_chain = ?error,
+                    "Skipping a confirmed subscriber. \
+                    Their stored contact details are invalid",
                 );
             }
         }
     }
     Ok(HttpResponse::Ok().finish())
+}
+
+struct ConfirmedSubscriber {
+    email: SubscriberEmail,
 }
 
 #[tracing::instrument(name = "Get confirmed subscribers", skip(pool))]
@@ -154,7 +220,7 @@ async fn get_confirmed_subscribers(
         SELECT email
         FROM subscriptions
         WHERE status = 'confirmed'
-    "#,
+        "#,
     )
     .fetch_all(pool)
     .await?
@@ -165,39 +231,4 @@ async fn get_confirmed_subscribers(
     })
     .collect();
     Ok(confirmed_subscribers)
-}
-
-struct Credentials {
-    username: String,
-    password: Secret<String>,
-}
-
-fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
-    // The header value, if present, must be a valid UTF8 string
-    let header_value = headers
-        .get("Authorization")
-        .context("The 'Authorization' header was missing")?
-        .to_str()
-        .context("The 'Authorization' header was not a valid UTF8 string.")?;
-    let base64encoded_segment = header_value
-        .strip_prefix("Basic ")
-        .context("The authorization scheme was not 'Basic'.")?;
-    let decoded_bytes = base64::decode_config(base64encoded_segment, base64::STANDARD)
-        .context("Failed to base64-decode 'Basic' credentials.")?;
-    let decoded_credentials = String::from_utf8(decoded_bytes)
-        .context("The decoded credential string is not valid UTF8.")?;
-    // Split into two segments, using ':' as delimiter
-    let mut credentials = decoded_credentials.splitn(2, ':');
-    let username = credentials
-        .next()
-        .ok_or_else(|| anyhow::anyhow!("A username must be provided in 'Basic' auth."))?
-        .to_string();
-    let password = credentials
-        .next()
-        .ok_or_else(|| anyhow::anyhow!("A password must be provided in 'Basic' auth."))?
-        .to_string();
-    Ok(Credentials {
-        username,
-        password: Secret::new(password),
-    })
 }
